dec04570eae0fc284e1ac650203f954991ba0232
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-02-04 14:07:40 -0600

    Implemented create task functionality.

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task-dto';
+import * as uuid from 'uuid/v1';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +9,19 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  createTask(creatTaksDto: CreateTaskDto): Task {
+    const { title, description } = creatTaksDto;
+
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
   }
 }
